f04c1daf67c3d53d117e91eb3d441296577a8c43
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2015-05-08 20:28:09 +0900

    Add test_load_required_config

--- tests/tests/backends/test_base.py
+++ tests/tests/backends/test_base.py
@@ -4,9 +4,30 @@ from __future__ import unicode_literals
 from django.test import TestCase
 
 from channels.backends.base import BaseChannel
+from channels.exceptions import ImproperlyConfigured
+
+
+class TestChannel(BaseChannel):
+    def send(self, message):
+        pass
 
 
 class BaseChannelTestCase(TestCase):
     def test_init(self):
         with self.assertRaises(TypeError):
             BaseChannel()
+
+    def test_load_required_config(self):
+        channel = TestChannel()
+        config = {
+            "URL": "http://example.com"
+        }
+        channel.load_required_config(config, {
+            "URL": "url"
+        })
+        self.assertEqual(channel.url, config["URL"])
+
+        with self.assertRaises(ImproperlyConfigured):
+            channel.load_required_config({}, {
+                "URL": "url"
+            })
