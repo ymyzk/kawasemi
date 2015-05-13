c13e40740cd5866cb2f6e1c4e40d640a55ea7de3
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2015-05-13 12:46:25 +0900

    Add HipChatChannelTestCase.test_send_fail

--- tests/tests/backends/test_hipchat.py
+++ tests/tests/backends/test_hipchat.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from copy import deepcopy
+
 from django.conf import settings
 from django.test import TestCase
 
 from channels.backends.hipchat import HipChatChannel
-from channels.exceptions import ImproperlyConfigured
+from channels.exceptions import HttpError, ImproperlyConfigured
 
 
 channels = settings.CHANNELS["CHANNELS"]
@@ -21,3 +23,13 @@ class HipChatChannelTestCase(TestCase):
 
     def test_send(self):
         self.channel.send("Test message")
+
+    def test_send_fail(self):
+        conf = deepcopy(config)
+        conf["TOKEN"] = "token"
+        channel = HipChatChannel(conf)
+
+        with self.assertRaises(HttpError):
+            channel.send("Test message", fail_silently=False)
+
+        channel.send("Test message", fail_silently=True)
