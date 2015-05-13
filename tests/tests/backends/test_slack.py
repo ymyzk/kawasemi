83a133d3e482179c7c6d89efdb195b1fac18b5ec
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2015-05-13 16:08:15 +0900

    Add SlackChannelTestCase.test_send_fail

--- tests/tests/backends/test_slack.py
+++ tests/tests/backends/test_slack.py
@@ -6,7 +6,7 @@ from django.conf import settings
 from django.test import TestCase
 
 from channels.backends.slack import SlackChannel
-from channels.exceptions import ImproperlyConfigured
+from channels.exceptions import HttpError, ImproperlyConfigured
 
 
 config = settings.CHANNELS["CHANNELS"]["channels.backends.slack.SlackChannel"]
@@ -28,3 +28,13 @@ class SlackChannelTestCase(TestCase):
 
     def test_send(self):
         self.channel.send("Test message")
+
+    def test_send_fail(self):
+        conf = deepcopy(config)
+        conf["URL"] = "https://hooks.slack.com/services/123/456/7890"
+        channel = SlackChannel(conf)
+
+        with self.assertRaises(HttpError):
+            channel.send("Test message", fail_silently=False)
+
+        channel.send("Test message", fail_silently=True)
