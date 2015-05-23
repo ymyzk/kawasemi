6ac53fd9cd03a174be3e1364354e2ec73448bd94
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2015-05-24 06:33:05 +0900

    Update tests settings

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -23,16 +23,18 @@ CHANNELS = {
             "API_ID": environ.get("CHANNELS_HIPCHAT_API_ID"),
             "TOKEN": environ.get("CHANNELS_HIPCHAT_TOKEN"),
             # Optional
-            "BASE_URL": environ.get("CHANNELS_HIPCHAT_BASE_URL", None)
+            "BASE_URL": environ.get("CHANNELS_HIPCHAT_BASE_URL"),
+            "COLOR": environ.get("CHANNELS_HIPCHAT_COLOR"),
+            "NOTIFY": environ.get("CHANNELS_HIPCHAT_NOTIFY") == "True"
         },
         "channels.backends.slack.SlackChannel": {
             # Required
             "URL": environ.get("CHANNELS_SLACK_URL"),
             # Optional
-            "USERNAME": environ.get("CHANNELS_SLACK_USERNAME", None),
-            "ICON_URL": environ.get("CHANNELS_SLACK_ICON_URL", None),
-            "ICON_EMOJI": environ.get("CHANNELS_SLACK_ICON_EMOJI", None),
-            "CHANNEL": environ.get("CHANNELS_SLACK_CHANNEL", None)
+            "USERNAME": environ.get("CHANNELS_SLACK_USERNAME"),
+            "ICON_URL": environ.get("CHANNELS_SLACK_ICON_URL"),
+            "ICON_EMOJI": environ.get("CHANNELS_SLACK_ICON_EMOJI"),
+            "CHANNEL": environ.get("CHANNELS_SLACK_CHANNEL")
         }
     }
 }
