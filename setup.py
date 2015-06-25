dfff977e074d56508323342cd448f6abfd13fb24
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2015-06-25 15:58:05 +0900

    Bump version number to 0.3.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
     from distutils.core import setup
 
 
-__version__ = '0.2.1'
+__version__ = '0.3.0'
 
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
