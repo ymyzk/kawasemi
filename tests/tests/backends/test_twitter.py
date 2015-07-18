2a401eb0700f2e74c872ce6e9f111e9943e7f72f
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2015-07-18 19:19:34 +0900

    Add tests for TwitterChannel

---
+++ tests/tests/backends/test_twitter.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+try:
+    from unittest import mock
+except ImportError:
+    import mock
+
+from django.conf import settings
+from django.test import TestCase
+import requests
+
+from channels.backends.twitter import TwitterChannel
+from channels.exceptions import HttpError
+
+
+channels = settings.CHANNELS["CHANNELS"]
+config = channels["channels.backends.twitter.TwitterChannel"]
+
+
+class TwitterChannelTestCase(TestCase):
+    def setUp(self):
+        self.channel = TwitterChannel(**config)
+
+    @mock.patch("requests.post")
+    def test_send(self, m):
+        response = requests.Response()
+        response.status_code = requests.codes.ok
+        m.return_value = response
+
+        self.channel.send("First tweet!")
+
+    @mock.patch("requests.post")
+    def test_send_fail_invalid_token(self, m):
+        response = requests.Response()
+        response.status_code = requests.codes.unauthorized
+        m.return_value = response
+
+        with self.assertRaises(HttpError):
+            self.channel.send("Test tweet", fail_silently=False)
+
+        self.channel.send("Test tweet", fail_silently=True)
