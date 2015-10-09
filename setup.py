70933aec1cd036470fe6379dc96cd76aaea80128
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2015-10-09 15:38:58 +0900

    Fix setup.py

--- setup.py
+++ setup.py
@@ -37,7 +37,6 @@ classifiers = [
     'Environment :: Web Environment',
     'Framework :: Django',
     'Intended Audience :: Developers',
-    'Operating System :: OS Independent',
     'License :: OSI Approved :: MIT License',
     'Operating System :: OS Independent',
     'Programming Language :: Python',
@@ -47,6 +46,7 @@ classifiers = [
     'Programming Language :: Python :: 3.2',
     'Programming Language :: Python :: 3.3',
     'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: Implementation :: PyPy',
     'Topic :: Internet :: WWW/HTTP',
 ]
