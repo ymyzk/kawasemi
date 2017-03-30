08401510e17caa1e7c44781d22c91f9676cbfdae
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2017-03-30 18:22:54 +0900

    Remove __future__.unicode_literals

--- kawasemi/__init__.py
+++ kawasemi/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from __future__ import absolute_import, unicode_literals
+from __future__ import absolute_import
 import sys
 from typing import Dict, Optional, Text  # noqa: F401
 
